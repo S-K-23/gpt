2bfa77fb3b456eaff30950a1b41ff92328f69a39
SohumK <109327670+S-K-23@users.noreply.github.com>
2026-05-18 18:41:58 -0500

    Edited model.h to match GPT. Added Parameters to model.c.

--- model.c
+++ model.c
@@ -1,2 +1,52 @@
-#include "model.h"
+#include <stdlib.h>
+#include <assert.h>
 
+#include "model.h"
+#include "random.c"
+
+// Token + Position Embeddings
+static float *wte, *d_wte;
+static float *wpe, *d_wpe;
+static float *lm_head, *d_lm_head;
+
+static float *attn_qry[N_LAYER], *d_attn_qry[N_LAYER]; //(128,32)
+static float *attn_key[N_LAYER], *d_attn_key[N_LAYER]; //(128,32)
+static float *attn_val[N_LAYER], *d_attn_val[N_LAYER]; //(128,32)
+static float *attn_out[N_LAYER], *d_attn_out[N_LAYER]; //(128,128)
+
+static float *mlp_exp[N_LAYER], *d_mlp_exp[N_LAYER]; //(128, 512)
+static float *mlp_con[N_LAYER], *d_mlp_con[N_LAYER]; //(512, 128)
+
+// Optimizer State
+static float *adam_m_wte, *adam_v_wte;
+static float *adam_m_wpe, *adam_v_wpe;
+static float *adam_m_lm, *adam_v_lm;
+
+static float *adam_m_qry[N_LAYER], *adam_v_qry[N_LAYER]; //(128,32)
+static float *adam_m_key[N_LAYER], *adam_v_key[N_LAYER]; //(128,32)
+static float *adam_m_val[N_LAYER], *adam_v_val[N_LAYER]; //(128,32)
+static float *adam_m_out[N_LAYER], *adam_v_out[N_LAYER]; //(128,128)
+
+static float *adam_m_exp[N_LAYER], *adam_v_exp[N_LAYER]; //(128, 512)
+static float *adam_m_con[N_LAYER], *adam_v_con[N_LAYER]; //(512, 128)
+
+static int num_params = 0;
+
+static float *make_param(int size, float std) {
+    float *param = (float *) calloc(size, sizeof(float));
+    assert(param);
+    
+    for (int i =0; i < size; i++) {
+        param[i] = random_gauss(0, std);
+    }
+
+    num_params += size;
+    return param;
+}
+
+static float *make_zero_param(int size) {
+    float *param = calloc(size, sizeof(float));
+    assert(param);
+
+    return (float *) param;
+}
--- model.h
+++ model.h
@@ -1,6 +1,6 @@
 #define N_EMBED 128
-#define N_HEAD 8
-#define N_LAYER 4
+#define N_HEAD 4
+#define N_LAYER 2
 #define CON_WINDOW 64
 #define HEAD_DIM (N_EMBED / N_HEAD)
 #define MLP_DIM (4 * N_EMBED)
