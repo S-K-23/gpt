1dd4e30a6789dbb840eac29b91e54c2b5c7bc839
SohumK <109327670+S-K-23@users.noreply.github.com>
2026-05-18 19:03:21 -0500

    Finished Initilize Parameters

--- model.c
+++ model.c
@@ -3,6 +3,7 @@
 
 #include "model.h"
 #include "random.c"
+#include "character_tokenizer.c"
 
 // Token + Position Embeddings
 static float *wte, *d_wte;
@@ -49,4 +50,62 @@ static float *make_zero_param(int size) {
     assert(param);
 
     return (float *) param;
+}
+
+static void init_params() {
+    int emb_s = vocab_size * N_EMBED;
+    int pemb_s = CON_WINDOW *  N_EMBED;
+    int attn_s = N_EMBED * N_EMBED;
+    int mlp_s = MLP_DIM * N_EMBED;
+
+    wte = make_param(emb_s, 0.02f);
+    d_wte = make_zero_param(emb_s);
+    adam_m_wte = make_zero_param(emb_s);
+    adam_v_wte = make_zero_param(emb_s);
+
+    wpe = make_param(pemb_s, 0.02f);
+    d_wpe = make_zero_param(pemb_s);
+    adam_m_wpe = make_zero_param(pemb_s);
+    adam_v_wpe = make_zero_param(pemb_s);
+
+    lm_head = make_param(emb_s, 0.02f);
+    d_lm_head = make_zero_param(emb_s);
+    adam_m_lm = make_zero_param(emb_s);
+    adam_v_lm = make_zero_param(emb_s);
+
+    printf("Loaded Token, Position, and LM Head");
+
+    for (int i = 0; i , N_LAYER; i++) {
+        attn_qry[i] = make_param(attn_s, 0.02f);
+        d_attn_qry[i] = make_zero_param(attn_s);
+        adam_m_qry[i] = make_zero_param(attn_s);
+        adam_v_qry[i] = make_zero_param(attn_s);
+
+        attn_key[i] = make_param(attn_s, 0.02f);
+        d_attn_key[i] = make_zero_param(attn_s);
+        adam_m_key[i] = make_zero_param(attn_s);
+        adam_v_key[i] = make_zero_param(attn_s);
+
+        attn_val[i] = make_param(attn_s, 0.02f);
+        d_attn_val[i] = make_zero_param(attn_s);
+        adam_m_val[i] = make_zero_param(attn_s);
+        adam_v_val[i] = make_zero_param(attn_s);
+
+        attn_out[i] = make_param(attn_s, 0.02f);
+        d_attn_out[i] = make_zero_param(attn_s);
+        adam_m_out[i] = make_zero_param(attn_s);
+        adam_v_out[i] = make_zero_param(attn_s);
+
+        mlp_exp[i] = make_param(mlp_s, 0.02f);
+        d_mlp_exp[i] = make_zero_param(mlp_s);
+        adam_m_exp[i] = make_zero_param(mlp_s);
+        adam_v_exp[i] = make_zero_param(mlp_s);
+    
+        mlp_con[i] = make_param(mlp_s, 0.02f);
+        d_mlp_con[i] = make_zero_param(mlp_s);
+        adam_m_con[i] = make_zero_param(mlp_s);
+        adam_v_con[i] = make_zero_param(mlp_s);
+    }
+
+    printf("Initialized with %d params\n", num_params);
 }
